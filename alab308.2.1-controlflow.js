a6a35ba34c03a79b940b4161f6768ef3f313bcfd
abdula96 <ahmed.abdula.hslc@gmail.com>
2024-10-22 18:23:38 -0400

    Define constants

---
+++ alab308.2.1-controlflow.js
@@ -0,0 +1,6 @@
+// Define constants
+const PI = 3.1415;
+const radius = 5; // radius of the circular area in meters
+const area = PI * radius * radius; // area of the circular garden
+const plantSpace = 0.8; // minimum space required for each plant in square meters
+const initialPlants = 20; // initial number of plants
